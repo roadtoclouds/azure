65873fc669f6a54fdc4c9dac2e070720b8ffe14e
roadtoclouds <57621665+roadtoclouds@users.noreply.github.com>
2020-03-09 16:51:44 +0530

    Update azure-bash-ansible.sh

--- azure-bash-ansible.sh
+++ azure-bash-ansible.sh
@@ -4,7 +4,7 @@
 sudo apt update
 sudo apt-get install -y ansible
 sed -i 's/#log_path/log_path/g' /etc/ansible/ansible.cfg
-sudo wget https://raw.githubusercontent.com/roadtoclouds/azure/master/ansible-test-playbook.yml -O /var/tmp
+sudo wget https://raw.githubusercontent.com/roadtoclouds/azure/master/ansible-test-playbook.yml -O /var/tmp/ansible-test-playbook.yml
 cd /var/tmp
 ansible-playbook -vv /var/tmp/ansible-test-playbook.yml >> /var/tmp/ansibleoutput
 echo "done done done" >> /var/tmp/donefile
